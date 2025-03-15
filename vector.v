ec3a34e17b5d80d64cf424d4e327a6ffdcfa24ea
SANJAY M <118831520+Sanjay-083@users.noreply.github.com>
2025-03-15 21:15:07 +0530

    Update vector.v

--- vector.v
+++ vector.v
@@ -1,7 +1,7 @@
 //In Verilog, vectors are used to handle multiple related signals under a single name, making it easier to manipulate and organize them.
 // A vector is essentially a multi-bit wire or register, where each bit in the vector can be accessed separately
 
-
+`default_nettype none 
 module top_module(
 	input [2:0] vec, 
 	output [2:0] outv,
@@ -23,3 +23,29 @@ endmodule
 //This Verilog module is designed to take a 3-bit input vector (vec), pass it directly to an output vector (outv),
 // and also split its individual bits into separate output signals (o2, o1, and o0).
 
+`default_nettype none     // Disable implicit nets. Reduces some types of bugs.
+module top_module( 
+    input wire [15:0] in,
+    output wire [7:0] out_hi,
+    output wire [7:0] out_lo );
+    
+    assign out_hi=in[15:8];
+    assign out_lo=in[7:0];
+
+endmodule
+
+//✅ in[15:8] picks the upper 8 bits
+//✅ in[7:0] picks the lower 8 bits
+
+
+module top_module( 
+    input [31:0] in,
+    output [31:0] out );
+
+     assign out[31:24] = in[7:0];    // Move Byte D to Byte A's position
+    assign out[23:16] = in[15:8];   // Move Byte C to Byte B's position
+    assign out[15:8]  = in[23:16];  // Move Byte B to Byte C's position
+    assign out[7:0]   = in[31:24];  // Move Byte A to Byte D's position
+
+endmodule
+//swap their order from A B C D → D C B A
